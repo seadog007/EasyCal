9872c2eb43a2e1cffb3dd5472d0a232f67a2fc9a
seadog007 <007seadog@gmail.com>
2015-01-01 07:49:11 +0800

    Added config example

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,6 @@
 var clientId = '148169895012-fghq2veobte2tarq7pa886jenj6t477s.apps.googleusercontent.com';
 var apiKey = 'AIzaSyCsT6wgwej7DuYNHCpqRJSvF5r8pqvB2wY';
 var scopes = 'https://www.googleapis.com/auth/calendar';
-var calendarId = 'k3hj32d7uemqf7fke49rfi40t4@group.calendar.google.com';
+var calendarId = 'Your Calendar ID';
 var maxResults = 50 //This value can never be larger than 2500 events. Optional.
 var AppName= 'EasyCal';
